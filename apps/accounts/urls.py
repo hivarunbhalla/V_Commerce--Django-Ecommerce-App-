314f327b0fa90c070191f3beff490962e61b6f7f
Varun Bhalla <106589945+hivarunbhalla@users.noreply.github.com>
2024-07-17 21:54:49 +0530

    Implemented registration process

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns= [
     path("login/", view= views.login_view, name= "login"),
+    path('register/' , views.register_page , name="register"),
+    path('activate/<email_token>/' , views.activate_email , name="activate_email"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-# Create your views here.
 
 def login_view(request):
     return render(request=request, template_name= 'accounts/login.html')
+
+
+def register_page(request):
+
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        user = User.objects.filter( email = email)
+
+        if user.exists():
+            messages.warning(request, 'User with same email alredy exists. You will be redirecting to login page')
+
+            return redirect('login')
+            
+
+        user = User.objects.create(
+            first_name= first_name,
+            last_name = last_name,
+            email = email,
+            username = email,
+        )
+        user.set_password(password)
+        user.save()
+
+        messages.warning(request, "Check your inbox! A verification email sent to your mail")
+        return HttpResponseRedirect(request.path_info)
+
+    return render(request=request, template_name= 'accounts/register.html')
+
+
+def activate_email(request):
+    pass
